09653f5ce7751f5bc5e0e4a94c26b6ba82a834bd
konstantinmakarenko <konmak@email.cz>
2025-01-19 14:18:38 +0600

    Initial commit: Add new files

--- Main.java
+++ Main.java
@@ -1,7 +1,34 @@
 public class Main {
-	
-	public static void main (String[] args) {
-		System.out.println("This is the test program");
+	public static void main(String[] args) {
+		Human human = new Human("Hero", 100, 15);
+		int zombiesKilled = 0;
+		int encounterCounter = 0;
+		Walking walk = new Walking();
+
+		while(human.isAlive() && zombiesKilled < 5){
+			System.out.println(walk.walkMessage());
+			encounterCounter++;
+			if (encounterCounter % 3 == 0){
+				Zombie zombie = new Zombie("Zombie", 50, 10);
+				System.out.println("You encountered: " + zombie.getName());
+				Battle battle = new Battle();
+				boolean result = battle.fight(human, zombie);
+				if (result){
+					zombiesKilled++;
+					System.out.println("You killed a zombie! " + zombiesKilled + "/5.");
+				}else{
+					System.out.println("You have been killed!");
+					break;
+				}
+			}
+			if (zombiesKilled == 5){
+				System.out.println("Congratulations! You have killed 5 zombies and won the game!");
+				break;
+			}
+		}
+		if (!human.isAlive()){
+			System.out.println("You lost the game!");
+		}
+
 	}
 }
-
